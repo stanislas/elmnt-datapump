05d6a0a402abfdf4c6292a39f053fff011172acc
Stanislas Nanchen <stan@hood.ch>
2015-04-19 11:49:17 +0200

    Release 0.1.0
    Signed-off-by: Stanislas Nanchen <stan@hood.ch>

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject
-	oracle-datapump-pkg-gen "0.1.0-SNAPSHOT"
+	ch.hood/oracle-datapump-pkg-gen "0.1.0"
 	:description "FIXME: write description"
 	:url "http://example.com/FIXME"
 	:license {:name "Eclipse Public License"
@@ -7,7 +7,8 @@
 	:dependencies [[org.clojure/clojure "1.6.0"]
 								 [prismatic/schema "0.4.0"]]
 	:profiles {:dev {:dependencies [[junit/junit "4.11"]]}}
-	:plugins [[lein-junit "1.1.8"]]
+	:plugins [[lein-junit "1.1.8"]
+						[lein-localrepo "0.5.3"]]
 	:source-paths ["src/clojure"]
 	:java-source-paths ["src/java" "test/java"]
 	:test-paths ["test/clojure"]
