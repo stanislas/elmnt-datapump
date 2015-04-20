d2f5c7b59f14055a9b9054cd0461e27dd5378262
Stanislas Nanchen <stanislas.nanchen@ergon.ch>
2015-04-20 08:17:44 +0200

    Release 0.2.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject
-	ch.hood/oracle-datapump-pkg-gen "0.2.0"
+	ch.hood/oracle-datapump-pkg-gen "0.2.1"
 	:description "FIXME: write description"
 	:url "http://example.com/FIXME"
 	:license {:name "Eclipse Public License"
