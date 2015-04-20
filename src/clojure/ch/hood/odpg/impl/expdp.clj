93360011e5b391e6702f13089ab4827f005ccc49
Stanislas Nanchen <stanislas.nanchen@ergon.ch>
2015-04-20 10:34:53 +0200

    Remove rendering ctx: only basic rendering needed.

--- src/clojure/ch/hood/odpg/impl/expdp.clj
+++ src/clojure/ch/hood/odpg/impl/expdp.clj
@@ -19,7 +19,7 @@
 	 :directory                    s/Str
 	 (s/optional-key :remote-link) s/Str})
 
-(defn render-schema-metadatafilter [ctx schemas]
+(defn render-schema-metadatafilter [schemas]
 	(let [schema-names (keys schemas)
 				schema-names (map double-quote schema-names)
 				schema-names (str/join ", " schema-names)]
@@ -35,7 +35,7 @@
 		:exclude-tables "NOT IN"))
 
 (defn render-table-metadatafile
-	([ctx filter-type table-names]
+	([filter-type table-names]
 	 (let [table-names (map double-quote table-names)
 				 table-names (str/join ", " table-names)]
 		 (str "dbms_datapump.metadata_filter("
@@ -44,15 +44,15 @@
 					"value => '" (filter-type-operator filter-type) " (" table-names ")', "
 					"object_type => 'TABLE'"
 					");")))
-	([ctx schemas]
+	([schemas]
 		;; if we come here, we have the guarantee of a single schema mode. see validate.
 	 (let [filter-types [:include-tables :exclude-tables]
 				 tables ((apply juxt filter-types) (get schemas (-> schemas keys first)))
-				 tables (map #(when (not (nil? %2)) (render-table-metadatafile ctx %1 %2)) filter-types tables)
+				 tables (map #(when (not (nil? %2)) (render-table-metadatafile %1 %2)) filter-types tables)
 				 tables (filter (comp not nil?) tables)]
 		 (str/join "\n" tables))))
 
-(defn render-subquery-datafilter [ctx schema-name table-name subquery]
+(defn render-subquery-datafilter [schema-name table-name subquery]
 	(str "dbms_datapump.data_filter("
 			 "handle => handle, "
 			 "name => 'SUBQUERY', "
@@ -61,16 +61,16 @@
 			 "schema_name => '" schema-name "'"
 			 ");"))
 
-(defn render-subqueries-datafilter [ctx schema-name tables]
+(defn render-subqueries-datafilter [schema-name tables]
 	(->> tables
-			 (map #(render-subquery-datafilter ctx schema-name (first %) (second %)))
+			 (map #(render-subquery-datafilter schema-name (first %) (second %)))
 			 (str/join "\n")))
 
-(defn render-schemas [ctx {:keys [schemas]}]
-	(let [metadata-schema-filter (render-schema-metadatafilter ctx schemas)
-				table-metadata-filter (render-table-metadatafile ctx schemas)
+(defn render-schemas [{:keys [schemas]}]
+	(let [metadata-schema-filter (render-schema-metadatafilter schemas)
+				table-metadata-filter (render-table-metadatafile schemas)
 				subquery-filters (into {} (map #(vector (first %) (:subquery-filters (second %))) schemas))
-				data-filters (map #(render-subqueries-datafilter ctx (first %) (second %)) subquery-filters)]
+				data-filters (map #(render-subqueries-datafilter (first %) (second %)) subquery-filters)]
 		(str metadata-schema-filter "\n" table-metadata-filter "\n"
 				 (str/join "\n" data-filters))))
 
@@ -85,7 +85,7 @@
 		:log-file "dbms_datapump.KU$_FILE_TYPE_LOG_FILE"
 		:dump-file "dbms_datapump.KU$_FILE_TYPE_DUMP_FILE"))
 
-(defn render-add-file [ctx directory file-type file-basename]
+(defn render-add-file [directory file-type file-basename]
 	(str "dbms_datapump.add_file("
 			 "handle => handle, "
 			 "filename => '" (str file-basename (file-type-suffix file-type)) "', "
@@ -99,7 +99,7 @@
 				 (.get cal Calendar/MONTH) "_"
 				 (.get cal Calendar/DAY_OF_MONTH))))
 
-(defn render-header [ctx {:keys [remote-link file-prefix directory]}]
+(defn render-header [{:keys [remote-link file-prefix directory]}]
 	(let [file-basename (str file-prefix "_" (render-today))]
 		(str/join "\n"
 							["declare"
@@ -108,8 +108,8 @@
 							 "begin"
 							 (str "handle := dbms_datapump.open('EXPORT', 'SCHEMA'"
 										(if (nil? remote-link) "" (str ", remote_link => " (single-quote remote-link))) ");")
-							 (render-add-file ctx directory :dump-file file-basename)
-							 (render-add-file ctx directory :log-file (str file-basename "_exp"))
+							 (render-add-file directory :dump-file file-basename)
+							 (render-add-file directory :log-file (str file-basename "_exp"))
 							 ])))
 
 (defn validate [exp-data]
@@ -124,11 +124,10 @@
 (s/defn render-exp-script
 	([exp-data :- ExpData]
 		(validate exp-data)
-		(let [ctx nil]
-			(str/join "\n"
-								[(render-header ctx exp-data)
-								 (render-schemas ctx exp-data)
-								 (c/render-footer)])))
+		(str/join "\n"
+							[(render-header exp-data)
+							 (render-schemas exp-data)
+							 (c/render-footer)]))
 	([file exp-data :- ExpData]
 		(let [script (render-exp-script exp-data)]
 			(spit file script))))
@@ -146,9 +145,9 @@
 
 	(println (render-exp-script input))
 
-	(render-subqueries-datafilter nil "PERGO" {
-																						 "BOOKING_SETTER" "WHERE POINT_OF_SALE in ('2001', '200')"
-																						 "BOOKING_DELTA"  "WHERE Pth = 231"
-																						 })
+	(render-subqueries-datafilter "PERGO" {
+																				 "BOOKING_SETTER" "WHERE POINT_OF_SALE in ('2001', '200')"
+																				 "BOOKING_DELTA"  "WHERE Pth = 231"
+																				 })
 
-	(render-schema-metadatafilter nil (:schemas input)))
+	(render-schema-metadatafilter (:schemas input)))
